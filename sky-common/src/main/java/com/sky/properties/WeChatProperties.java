02832de48749fb65e14d2fc37d3a67c1e7e05aa2
n0thing <1654151425@qq.com>
2024-12-21 21:07:47 +0800

    新增员工业务代码开发

--- sky-common/src/main/java/com/sky/properties/WeChatProperties.java
+++ sky-common/src/main/java/com/sky/properties/WeChatProperties.java
@@ -20,4 +20,75 @@ public class WeChatProperties {
     private String notifyUrl; //支付成功的回调地址
     private String refundNotifyUrl; //退款成功的回调地址
 
+    public String getAppid() {
+        return appid;
+    }
+
+    public void setAppid(String appid) {
+        this.appid = appid;
+    }
+
+    public String getSecret() {
+        return secret;
+    }
+
+    public void setSecret(String secret) {
+        this.secret = secret;
+    }
+
+    public String getMchid() {
+        return mchid;
+    }
+
+    public void setMchid(String mchid) {
+        this.mchid = mchid;
+    }
+
+    public String getMchSerialNo() {
+        return mchSerialNo;
+    }
+
+    public void setMchSerialNo(String mchSerialNo) {
+        this.mchSerialNo = mchSerialNo;
+    }
+
+    public String getPrivateKeyFilePath() {
+        return privateKeyFilePath;
+    }
+
+    public void setPrivateKeyFilePath(String privateKeyFilePath) {
+        this.privateKeyFilePath = privateKeyFilePath;
+    }
+
+    public String getApiV3Key() {
+        return apiV3Key;
+    }
+
+    public void setApiV3Key(String apiV3Key) {
+        this.apiV3Key = apiV3Key;
+    }
+
+    public String getWeChatPayCertFilePath() {
+        return weChatPayCertFilePath;
+    }
+
+    public void setWeChatPayCertFilePath(String weChatPayCertFilePath) {
+        this.weChatPayCertFilePath = weChatPayCertFilePath;
+    }
+
+    public String getNotifyUrl() {
+        return notifyUrl;
+    }
+
+    public void setNotifyUrl(String notifyUrl) {
+        this.notifyUrl = notifyUrl;
+    }
+
+    public String getRefundNotifyUrl() {
+        return refundNotifyUrl;
+    }
+
+    public void setRefundNotifyUrl(String refundNotifyUrl) {
+        this.refundNotifyUrl = refundNotifyUrl;
+    }
 }
